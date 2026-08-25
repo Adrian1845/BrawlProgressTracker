10da9c861275eabf7230b0af0f22088e7e45960f
Adrian1845 <adrifb188@gmail.com>
2026-08-25 18:46:43 +0200

    make project as a static page

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    maxDuration: 10
-  })
+  output: 'static'
 });
